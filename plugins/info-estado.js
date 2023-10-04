2fc98c29d9d175f59a76cd6feea6951903122fe8
xalocc <146940476+xalocc@users.noreply.github.com>
2023-10-04 05:53:01 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn}) => {
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const str = `╭────[ *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* ]
+    const str = `╭────[ *@teamfg.mixto* ]
 │
 │ *➤ ʜᴏʟᴀ ${taguser}*
 │
